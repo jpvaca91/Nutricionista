644a498de8a4dbc2c60774afc2ab383e618581c4
jpvaca91 <vacajuanpablo@gmail.com>
2023-10-02 20:55:24 -0300

    Metodo guardar paciente

--- src/nutricionista/AccesoADatos/PacienteData.java
+++ src/nutricionista/AccesoADatos/PacienteData.java
@@ -6,16 +6,48 @@
 package nutricionista.AccesoADatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import nutricionista.Entidades.Paciente;
+import org.mariadb.jdbc.Statement;
 
 public class PacienteData {
 
     private Connection con = null;
-    
-    public PacienteData(){
-        con =   Coneccion.getConexion();
-        
+
+    public PacienteData() {
+        con = Coneccion.getConexion();
+
     }
 
-    
-    
+    public void guardarPaciente(Paciente paciente) {
+        String sql = "INSERT INTO paciente (nombre,dni,domicilio,telefono,estado)"
+                + "VALUE (?,?,?,?,?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, paciente.getNombre());
+            ps.setInt(2, paciente.getDni());
+            ps.setString(3, paciente.getDomicilio());
+            ps.setString(4, paciente.getTelefono());
+            ps.setBoolean(5, paciente.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                paciente.setIdPaciente(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Paciente guardado");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la Base de Datos"+ex);
+        }
+
+    }
+
 }
