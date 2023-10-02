5541f8d9dad831dcf4d38eae9a7040dac17ecc24
norpablo <norpablocabrera25@gmail.com>
2023-10-02 20:06:37 -0300

    paciente entidades

--- src/nutricionista/Entidades/Comida.java
+++ src/nutricionista/Entidades/Comida.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nutricionista.Entidades;
 
-/**
- *
- * @author pablo
- */
+
 public class Comida {
+    
+    private String comida;
+    private 
+    
     
 }
--- src/nutricionista/Entidades/Paciente.java
+++ src/nutricionista/Entidades/Paciente.java
@@ -3,5 +3,71 @@ package nutricionista.Entidades;
 
 
 public class Paciente {
+ 
+    private String nombre;
+    private int dni;
+    private String domicilio;
+    private String telefono;
+    private int idPaciente;
 
+    public Paciente() {
+    }
+
+    public Paciente(String nombre, int dni, String domicilio, String telefono, int idPaciente) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.idPaciente = idPaciente;
+    }
+
+    public Paciente(String nombre, int dni, String domicilio, String telefono) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getIdPaciente() {
+        return idPaciente;
+    }
+
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
+    
+    
+    
 }
